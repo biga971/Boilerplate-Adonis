a0eb2b2431dd823ea0bb3c548766d5a077cfa2b9
biga971 <enlive1.eb@gmail.com>
2024-04-24 12:07:12 -0400

    first commit

---
+++ app/controllers/auth/auth_facebook_controller.ts
@@ -0,0 +1,27 @@
+// Importez le nouveau service
+import SocialAuthService from '#services/auth/social_auth_service'
+import { HttpContext } from '@adonisjs/core/http'
+
+export default class AuthFacebooksController {
+  async redirect({ ally }: HttpContext) {
+    await ally.use('facebook').redirect((request: any) => {
+      request.scopes(['public_profile', 'email'])
+    })
+  }
+
+  async handleCallback({ ally, response }: HttpContext) {
+    const facebookUser = ally.use('facebook')
+
+    const result = await SocialAuthService.handleFacebookCallback(facebookUser)
+
+    if (typeof result === 'string') {
+      return response.badRequest({ message: result })
+    }
+
+    console.log(result)
+    return response.ok({
+      token: result.token,
+      ...result.user.serialize(),
+    })
+  }
+}
